7949694215b8f994abe9c50479ac24c8ebe60477
Sarah-devspace <saragedamu292@gmail.com>
2026-07-18 20:56:36 +0300

    Add lyrics to hello.c

---
+++ hello.c
@@ -0,0 +1,2 @@
+see u hello hello ohhh hit me like ray of son u the only the one i want and am addicted to your love gravity can't forget 
+it;s like i have been walking every had broked babdy i can feel ur hello
